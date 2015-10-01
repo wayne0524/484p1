5647838a1fc7120ba233279a7758bae26be40bf7
Wayne <icharles@umich.edu>
2015-09-30 22:58:55 -0400

    more relations added

--- createTables.sql
+++ createTables.sql
@@ -1,7 +1,7 @@
 
 -- User Information--
 CREATE TABLE Users
-   ( userid INTEGER NOT NULL, 
+   ( userid INTEGER, 
 	 fname CHAR(20) NOT NULL, 
 	 lname CHAR(20) NOT NULL,
 	 yob INTEGER NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE Users
 
 -- Create LLLLocation--
 CREATE TABLE Location
-	(locid INTEGER ,
+	(locid INTEGER,
 	 country CHAR(30),
 	 state CHAR(30),
 	 city CHAR(30),
@@ -21,17 +21,24 @@ CREATE TABLE Location
 	 --PRIMARY KEY(locid, userid));
 	
 -- Create Table of hometown of Users--
-CREATE TABLE Address
+CREATE TABLE Currenthome
 	(userid INTEGER,
- 	homelocid INTEGER,
- 	currentlocid INTEGER,
- 	FOREIGN KEY(userid) REFERENCES Users);
+ 	 currentlocid INTEGER,
+ 	 FOREIGN KEY(userid) REFERENCES Users,
+ 	 FOREIGN KEY(currentlocid) REFERENCES Location(locid),
+ 	 PRIMARY KEY(userid));
 
+CREATE TABLE Hometown
+	(userid INTEGER,
+ 	 hometownlocid INTEGER,
+ 	FOREIGN KEY(userid) REFERENCES Users,
+ 	FOREIGN KEY(hometownlocid) REFERENCES Location(locid),
+ 	PRIMARY KEY(userid));
 
 
 -- Event --
 CREATE TABLE Event
-	(eventid INTEGER NOT NULL,
+	(eventid INTEGER,
 	 eventname CHAR(30),
 	 tagline CHAR(30),
 	 description CHAR(30),
@@ -40,24 +47,37 @@ CREATE TABLE Event
 	 subtype CHAR(20),
 	 time_start TIMESTAMP,
 	 time_end TIMESTAMP,
-	 locid INTEGER,
+	 --locid INTEGER,
 	 location CHAR(30),
-	 creator INTEGER,
-	 FOREIGN KEY(locid) REFERENCES Location,
-	 FOREIGN KEY(creator) REFERENCES Users(userid),
+	 --FOREIGN KEY(locid) REFERENCES Location,
+	 --FOREIGN KEY(creator) REFERENCES Users(userid),
 	 PRIMARY KEY(eventid));
 
+-- Event being helded at location
+CREATE TABLE Holds
+    	(locid INTEGER,
+		 eventid INTEGER,
+		FOREIGN KEY(locid) REFERENCES Location,
+		FOREIGN KEY(eventid) REFERENCES Event,
+	    PRIMARY KEY(locid, eventid));
+
+
+CREATE TABLE Creator
+    	(creatorid INTEGER,
+		 eventid INTEGER,
+		FOREIGN KEY(creatorid) REFERENCES Users(userid),
+		FOREIGN KEY(eventid) REFERENCES Event,
+	    PRIMARY KEY(creatorid, eventid));
 
 
 -- Table list of all participants, including eventid AND staus --
 CREATE TABLE Participants
     	(pptid INTEGER,
-		eventid INTEGER,
-		status CHAR(20),
+		 eventid INTEGER,
+		 status CHAR(20),
 		FOREIGN KEY(pptid) REFERENCES Users(userid),
 		FOREIGN KEY(eventid) REFERENCES Event,
 	    PRIMARY KEY(pptid, eventid));
-
 
 -- Photo --
 CREATE TABLE Photo
@@ -69,6 +89,7 @@ CREATE TABLE Photo
 	 --albumid INTEGER,
 	 --KEY(albumid) REFERENCES Album,  added back at end
 	 PRIMARY KEY(photoid));
+
 
 
 -- Album --
@@ -86,47 +107,88 @@ CREATE TABLE Album
 	  PRIMARY KEY(albumid));
 
 
-
--- Tags in photos--
-
+-- Relation: Tags in photos--
 CREATE TABLE Tags
-	(albumid INTEGER,
+	(photoid INTEGER,
 	 userid INTEGER,
 	 coo_x INTEGER,
 	 coo_y INTEGER,
 	 time_tag TIMESTAMP,
-	 FOREIGN KEY(albumid) REFERENCES Album,
+	 FOREIGN KEY(photoid) REFERENCES Photo,
 	 FOREIGN KEY(userid) REFERENCES Users,
-	 PRIMARY KEY(albumid, userid));
+	 PRIMARY KEY(photoid, userid));
+
+-- Relation: between photo and Album ---
+CREATE TABLE Belongs
+	(photoid INTEGER,
+	 albumid INTEGER,
+	 FOREIGN KEY(photoid) REFERENCES Photo,
+	 FOREIGN KEY(albumid) REFERENCES Album,
+	 PRIMARY KEY(photoid, albumid));
+
+
+
+-- Relation  : Album owned by one user --
+CREATE TABLE Owns
+	(albumid INTEGER,
+	 userid INTEGER,
+	 FOREIGN KEY(userid) REFERENCES Users,
+	 FOREIGN KEY(albumid) REFERENCES Album,
+	 PRIMARY KEY(userid, albumid));
+
+
 
 -- Education  DO WE NEED A EDUCATION ID???????????????????????????????????????????--
 CREATE TABLE Education
-	(userid INTEGER,
+	(eduid INTEGER,
 	 inst_name CHAR(20),	
 	 duration INTEGER,
 	 degree CHAR(20),
 	 concentration CHAR(20),	
-	 FOREIGN KEY(userid) REFERENCES Users,
-	 PRIMARY KEY(userid));
+	 PRIMARY KEY(eduid));
 
 
+CREATE TABLE Attend
+	(eduid INTEGER,
+	 --userid INTEGER,
+	userid INTEGER,	
+	FOREIGN KEY(userid) REFERENCES Users,
+	PRIMARY KEY(eduid));
 
 -- MEssage 
 CREATE TABLE Messages
-	( messageid INTEGER,
-	  sender INTEGER,
-	  content CHAR(30),
-	  time TIMESTAMP,
-	  FOREIGN KEY(sender) REFERENCES Users(userid),
-	  PRIMARY KEY(messageid));	
+	(messageid INTEGER,
+	 time TIMESTAMP,
+	 --sender INTEGER,
+	 content CHAR(30),
+	 --FOREIGN KEY(sender) REFERENCES Users(userid),
+	 PRIMARY KEY(messageid));	
 
+
+CREATE TABLE Sender
+	(messageid INTEGER,
+	--timesend TIMESTAMP,
+	userid INTEGER,
+	--FOREIGN KEY(timesend) REFERENCES Messages(time),
+	FOREIGN KEY(messageid) REFERENCES Messages,
+	FOREIGN KEY(userid) REFERENCES Users,
+	PRIMARY KEY(messageid,userid));
 
 CREATE TABLE Receiver
-	(	messageid INTEGER,
-		userid INTEGER,
-		FOREIGN KEY(messageid) REFERENCES Messages,
-		FOREIGN KEY(userid) REFERENCES Users,
-		PRIMARY KEY(messageid));
+	(messageid INTEGER,
+	--timesend TIMESTAMP,
+	userid INTEGER,
+	--FOREIGN KEY(timesend) REFERENCES Messages(time),
+	FOREIGN KEY(messageid) REFERENCES Messages,
+	FOREIGN KEY(userid) REFERENCES Users,
+	PRIMARY KEY(messageid,userid));
+	
+-- CREATE TABLE Receiver
+-- 	(time TIMESTAMP,
+-- 	 userid INTEGER,
+-- 	FOREIGN KEY(time) REFERENCES Messages,
+-- 	FOREIGN KEY(userid) REFERENCES Users,
+-- 	PRIMARY KEY(time,userid));
 
 
 
@@ -135,4 +197,7 @@ ALTER TABLE Photo
 	ADD (albumid INTEGER, 
 		FOREIGN KEY(albumid) REFERENCES Album);
 
+-- ALTER TABLE Messages
+-- 	ADD (sender INTEGER, 
+-- 		FOREIGN KEY(userid) REFERENCES Sender);
 
--- dropTables.sql
+++ dropTables.sql
@@ -1,13 +1,26 @@
 DROP TABLE Receiver;
+DROP TABLE Sender;
+
+-- ALTER TABLE Messages
+-- 	DROP COLUMN sender;
+
 DROP TABLE Messages;
+DROP TABLE Attend;
 DROP TABLE Education;
+DROP TABLE Belongs;
+DROP TABLE Owns;
 DROP TABLE Tags;
+
 ALTER TABLE Photo
-	DROP COLUMN Albumid ;
+	DROP COLUMN Albumid;
+
 DROP TABLE Album;
 DROP TABLE Photo;
 DROP TABLE Participants;
+DROP TABLE Creator;
+DROP TABLE Holds;
 DROP TABLE Event;
-DROP TABLE Address;
+DROP TABLE Hometown;
+DROP TABLE Currenthome;
 DROP TABLE Location;
 DROP TABLE Users;
